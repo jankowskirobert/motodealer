32772285a6d7372464252bc1e67a5fc6c118bd19
Robert Jankowski <jankowski.r@hotmail.com>
2019-06-22 21:44:31 +0200

    Camel route as web api

---
+++ src/main/java/com/jvmless/shop/base/RoutesConfiguration.java
@@ -0,0 +1,39 @@
+package com.jvmless.shop.base;
+
+import com.jvmless.shop.sales.application.command.ProductReservationCommand;
+import com.jvmless.shop.sales.application.handler.ProductReservationCommandHandler;
+import com.jvmless.shop.sales.application.route.ReservationRoute;
+import com.jvmless.shop.sales.domain.productcatalog.ProductRepository;
+import com.jvmless.shop.sales.domain.productcatalog.ProductReservationPolicyFactory;
+import com.jvmless.shop.sales.domain.reservation.ReservationRepository;
+import com.jvmless.shop.sales.domain.reservation.ReservationRuleFactory;
+import com.jvmless.shop.usermanagement.UserContextService;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConfiguration {
+
+    @Bean
+    public ProductReservationCommandHandler productReservationCommandHandler(
+            ProductRepository productRepository,
+            UserContextService userContextService,
+            ReservationRepository reservationRepository,
+            ProductReservationPolicyFactory productReservationPolicyFactory,
+            ReservationRuleFactory reservationRuleFactory
+
+    ) {
+        return new ProductReservationCommandHandler(productRepository,
+                userContextService,
+                reservationRepository,
+                productReservationPolicyFactory,
+                reservationRuleFactory
+        );
+    }
+
+    @Bean
+    public ReservationRoute reservationRoute(ProductReservationCommandHandler productReservationCommandHandler) {
+        return new ReservationRoute(productReservationCommandHandler);
+    }
+}
---
+++ src/main/java/com/jvmless/shop/sales/application/route/ReservationRoute.java
@@ -0,0 +1,28 @@
+package com.jvmless.shop.sales.application.route;
+
+import com.jvmless.shop.sales.application.command.ProductReservationCommand;
+import com.jvmless.shop.sales.application.handler.ProductReservationCommandHandler;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+public class ReservationRoute extends RouteBuilder {
+
+
+    private final ProductReservationCommandHandler productReservationCommandHandler;
+
+    public ReservationRoute(ProductReservationCommandHandler productReservationCommandHandler) {
+        this.productReservationCommandHandler = productReservationCommandHandler;
+    }
+
+    @Override
+    public void configure() throws Exception {
+
+        from("restlet:{{base.url}}/reserve?restletMethod=post")
+                .unmarshal()
+                .json(JsonLibrary.Jackson)
+                .process(exchange -> {
+                    productReservationCommandHandler.handle(exchange.getIn().getBody(ProductReservationCommand.class));
+                });
+
+    }
+}
